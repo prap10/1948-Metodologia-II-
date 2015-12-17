3cb27a3a82aeab50fef49c647271dbcf695f2487
Pablo Arroyo <prap10.96@gmail.com>
2015-12-16 22:40:11 -0600

    Guardar/Cargar partida para pruebas

--- Assets/__Scripts/CambioDeEscena.cs
+++ Assets/__Scripts/CambioDeEscena.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
+
 public class CambioDeEscena : MonoBehaviour {
 
 	public float tiempo;
 	public Text tiempoTexto;
 	public string escenaC;
 	public Text puntaje;
-	public void CambiarEscena(string escena){
-	Logo.puntos += 10;
+
+	public void CambiarEscena(string escena) {
+		if (escena.Equals ("Pantalla1Cartago"))
+			SaveData.pasoAlajuela = true;
+		if (escena.Equals ("Pantalla1SanJose"))
+			SaveData.pasoCartago = true;
+
+		SaveData.Puntaje += 10;
 		Application.LoadLevel (escena);
 	}
 
 	public void ErrorRespuesta(){
-		Logo.puntos -= 5;
+		SaveData.Puntaje -= 5;
 	}
 
 	void Start(){
@@ -26,9 +34,10 @@ public class CambioDeEscena : MonoBehaviour {
 			tiempo = 3;
 	}
 	void Update(){
-		puntaje.text = Logo.puntos.ToString();
+
+		puntaje.text = SaveData.Puntaje.ToString();
 		if (tiempo <= 0) {
-			Logo.puntos -= 5;
+			SaveData.Puntaje -= 5;
 			Application.LoadLevel (escenaC);
 		} else {
 			tiempoTexto.text = tiempo.ToString("f0");
